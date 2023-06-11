b5b1247771d8baac90c7e793ade341fd78e0e93b
Juan Uriarte <uriarte.juan@csu.fullerton.edu>
2023-06-11 09:07:21 -0700

    Created blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Juan Uriarte
+# uriarte.juan@csu.fullerton.edu
+# @uriartej
+
+import time
+from blackjackgame.game import BlackjackGame
+
+def main():
+    game = BlackjackGame()
+    game.play_game()
+
+if __name__ == "__main__":
+    main()
