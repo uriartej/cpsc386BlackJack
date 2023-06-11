a5f0f03fe1ebb898a30cf7726a57a91db9c39de1
Juan Uriarte <uriarte.juan@csu.fullerton.edu>
2023-06-11 09:12:50 -0700

    I added more methods such as display returns, play dealer turn, play again and reset game.

--- blackjackgame/game.py
+++ blackjackgame/game.py
@@ -4,7 +4,6 @@
 # @uriartej
 
 
-
 import time
 from .cards import Deck
 from .player import HumanPlayer, AIPlayer
@@ -41,4 +40,80 @@ class BlackjackGame:
             else:
                 print("Invalid input. Please enter a number between (1-4)")
 
+    def play_round(self):
+        for player in self.players:
+            self.deal_initial_cards(player)
 
+        for player in self.players:
+            self.play_player_turn(player)
+
+        self.play_dealer_turn()
+
+        self.display_results()
+
+    def deal_initial_cards(self, player):
+        for _ in range(2):
+            card = self.deck.draw_card()
+            player.add_card(card)
+
+    def play_player_turn(self, player):
+        print(f"\n{player.name}'s turn:")
+        while True:
+            print(player)
+            choice = input("Do you want to hit? (y/n): ")
+            if choice.lower() == 'y':
+                card = self.deck.draw_card()
+                player.add_card(card)
+                if player.get_hand_value() > 21:
+                    print(f"\n{player.name} busted!")
+                    break
+            elif choice.lower() == 'n':
+                break
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
+
+    def play_dealer_turn(self):
+        print("\nDealer's turn:")
+        dealer = self.players[-1]
+        while dealer.get_hand_value() < 17:
+            card = self.deck.draw_card()
+            dealer.add_card(card)
+            print(f"Dealer draws {card}")
+
+        if dealer.get_hand_value() > 21:
+            print("\nDealer busted!")
+
+    def display_results(self):
+        print("\nResults:")
+        for player in self.players[:-1]:
+            print(f"{player.name}: {player.get_hand_value()}")
+
+        dealer = self.players[-1]
+        dealer_value = dealer.get_hand_value()
+
+        for player in self.players[:-1]:
+            player_value = player.get_hand_value()
+            if player_value > 21:
+                print(f"{player.name} loses!")
+            elif dealer_value > 21:
+                print(f"{player.name} wins!")
+            elif player_value == dealer_value:
+                print(f"{player.name} pushes!")
+            elif player_value > dealer_value:
+                print(f"{player.name} wins!")
+            else:
+                print(f"{player.name} loses!")
+
+    def play_again(self):
+        while True:
+            choice = input("\nDo you want to play again? (y/n): ")
+            if choice.lower() == 'y':
+                return True
+            elif choice.lower() == 'n':
+                return False
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
+
+    def reset_game(self):
+        for player in self.players:
+            player
