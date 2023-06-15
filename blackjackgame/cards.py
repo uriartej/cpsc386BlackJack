0cf7b923dd0d0ccb7c7bc28db5a7c723fb5932c7
Juan Uriarte <uriarte.juan@csu.fullerton.edu>
2023-06-14 18:43:55 -0700

    Modified def shuffle(self).

--- blackjackgame/cards.py
+++ blackjackgame/cards.py
@@ -9,19 +9,18 @@ from collections import namedtuple
 
 Card = namedtuple("Card", ["rank", "suit"])
 
-
 class Deck:
-	def __init__(self):
-		self.cards = []
-		ranks = [str(num) for num in range(2, 11)] + ["J", "Q", "K", "A"]
-		suits = ["Clubs", "Hearts", "Spades", "Diamonds"]
+    def __init__(self):
+        self.cards = []
+        ranks = [str(num) for num in range(2, 11)] + ["J", "Q", "K", "A"]
+        suits = ["Clubs", "Hearts", "Spades", "Diamonds"]
 
-		for rank in ranks:	
-			for suit in suits:
-				self.cards.append(Card(rank, suit))
+        for rank in ranks:
+            for suit in suits:
+                self.cards.append(Card(rank, suit))
 
-	def shuffle(self):
-		random.shuffle(self.cards)
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-	def draw_card(self):
-		return self.cards.pop()
+    def draw_card(self):
+        return self.cards.pop()
