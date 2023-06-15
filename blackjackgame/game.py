f7baa7ffb8bb90c8cd1f24cca4547678b8c2b9bc
Juan Uriarte <uriarte.juan@csu.fullerton.edu>
2023-06-14 18:53:21 -0700

    Modified def playgame took off the for loop and added a while loop. I also took off the while loop on playround because it was giving me errors, added a for loop for initial cards.

--- blackjackgame/game.py
+++ blackjackgame/game.py
@@ -4,32 +4,29 @@
 # @uriartej
 
 
-import time
-from .cards import Deck
-from .player import HumanPlayer, AIPlayer
+from blackjackgame.cards import Deck
+from blackjackgame.player import Player
+
 
 class BlackjackGame:
     def __init__(self):
         self.players = []
         self.deck = None
-        self.dealer = None
-        self.cut_card_position = None
 
     def play_game(self):
         self.setup_game()
-        self.play_round()
-        while self.play_again():
-            self.reset_game()
+        while True:
             self.play_round()
+            if not self.play_again():
+                break
+            self.reset_game()
         self.end_game()
 
     def setup_game(self):
         num_players = self.get_num_players()
-        for i in range(1, num_players + 1):
-            player_name = input(f"Enter player {i} name: ")
-            self.players.append(HumanPlayer(player_name))
-
-        self.players.append(AIPlayer("Dealer"))
+        for i in range(num_players):
+            player_name = input(f"Enter player {i+1} name: ")
+            self.players.append(Player(player_name))
         self.deck = Deck()
 
     def get_num_players(self):
@@ -38,87 +35,81 @@ class BlackjackGame:
             if num_players.isdigit() and int(num_players) in range(1, 5):
                 return int(num_players)
             else:
-                print("Invalid input. Please enter a number between (1-4)")
+                print("Invalid input. Please enter a number between 1 and 4.")
 
     def play_round(self):
-        for player in self.players:
-            self.deal_initial_cards(player)
+        self.deck.shuffle()
+        self.deal_initial_cards()
+        self.display_all_hands()
 
         for player in self.players:
             self.play_player_turn(player)
 
         self.play_dealer_turn()
+        self.display_all_hands()
+        self.determine_results()
 
-        self.display_results()
-
-    def deal_initial_cards(self, player):
+    def deal_initial_cards(self):
         for _ in range(2):
-            card = self.deck.draw_card()
-            player.receive_card(card)
+            for player in self.players:
+                card = self.deck.draw_card()
+                player.add_card(card)
+
+    def display_all_hands(self):
+        for player in self.players:
+            player.display_hand()
 
     def play_player_turn(self, player):
-        print(f"\nPlayer's turn:")
-        card_number = 1
+        print(f"{player.name}'s turn:")
         while True:
-            print(player)
-            choice = input("Do you want to hit? (y/n): ")
-            if choice.lower() == 'y':
-                card = self.deck.draw_card()
-                player.receive_card(card)
-                if player.get_hand_value() > 21:
-                    print(f"\nCard {card_number}: {card.rank} of {card.suit}")
-                    print(f"Player busted!")
+            choice = input(f"{player.name}, do you want to hit? (yes/no): ")
+            if choice.lower() == "yes":
+                self.hit(player)
+                self.display_all_hands()
+                if self.is_bust(player):
+                    print(f"{player.name} busts!")
                     break
-                else:
-                    print(f"\nCard {card_number}: {card.rank} of {card.suit}")
-                    card_number += 1
-            elif choice.lower() == 'n':
+            elif choice.lower() == "no":
                 break
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
 
+    def hit(self, player):
+        card = self.deck.draw_card()
+        player.add_card(card)
+
+    def is_bust(self, player):
+        return player.get_hand_value() > 21
 
     def play_dealer_turn(self):
-        print("\nDealer's turn:")
-        dealer = self.players[-1]
+        print("Dealer's turn:")
+        dealer = Player("Dealer")
+        dealer.display_hand()
         while dealer.get_hand_value() < 17:
-            card = self.deck.draw_card()
-            dealer.receive_card(card)
-            print(f"Dealer draws {card}")
+            self.hit(dealer)
+            dealer.display_hand()
+            if self.is_bust(dealer):
+                print("Dealer busts!")
+                break
 
-        if dealer.get_hand_value() > 21:
-            print("\nDealer busted!")
+    def determine_results(self):
+        dealer = Player("Dealer")
+        dealer_total = dealer.get_hand_value()
 
-    def display_results(self):
-        print("\nResults:")
-        for player in self.players[:-1]:
-            print(f"{player.name}: {player.get_hand_value()}")
-
-        dealer = self.players[-1]
-        dealer_value = dealer.get_hand_value()
-
-        for player in self.players[:-1]:
-            player_value = player.get_hand_value()
-            if player_value > 21:
+        for player in self.players:
+            player_total = player.get_hand_value()
+            if self.is_bust(player):
+                print(f"{player.name} busts!")
+            elif self.is_bust(dealer):
+                print(f"{player.name} wins!")
+            elif player_total > dealer_total:
+                print(f"{player.name} wins!")
+            elif player_total < dealer_total:
                 print(f"{player.name} loses!")
-            elif dealer_value > 21:
-                print(f"{player.name} wins!")
-            elif player_value == dealer_value:
-                print(f"{player.name} pushes!")
-            elif player_value > dealer_value:
-                print(f"{player.name} wins!")
             else:
-                print(f"{player.name} loses!")
+                print(f"{player.name} pushes!")
 
     def play_again(self):
-        while True:
-            choice = input("\nDo you want to play again? (y/n): ")
-            if choice.lower() == 'y':
-                return True
-            elif choice.lower() == 'n':
-                return False
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
+        choice = input("Do you want to play again? (yes/no): ")
+        return choice.lower() == "yes"
 
     def reset_game(self):
         for player in self.players:
@@ -126,4 +117,8 @@ class BlackjackGame:
         self.deck = Deck()
 
     def end_game(self):
-        print("Game Over")
+        print("Game over. Thanks for playing!")
+
+
+
+
